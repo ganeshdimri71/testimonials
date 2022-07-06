34192450f873fe68028687091c53d0a82e6450cb
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-07-06 05:45:38 +0530

    Hello

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import './App.css'
 import $ from 'jquery';
-import image1 from './images/21.jpg'
-import image2 from './images/3.jpg'
-import image3 from './images/4.jpg'
-import image4 from './images/6.jpg'
+import image1 from './images/syed.png'
 function initParadoxWay() {
   "use strict";
 
@@ -82,6 +79,75 @@ const App = () => {
             <div className="swiper-wrapper">
               <div className="swiper-slide">
                 <div className="testi-item">
+                  <div className="testi-avatar"><img src='https://avatars.githubusercontent.com/u/64885201?s=64&v=4' /></div>
+                  <div className="testimonials-text-before"><i className="fa fa-quote-right"></i></div>
+                  <div className="testimonials-text">
+                    <div className="listing-rating">
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                    </div>
+                    <p>I have been working here since 3 years. I would like to say that this is one of the one company that you people can work with and enhance your future.I love this company so much that I would be working here till my last breath.</p>
+                    <a href="#" className="text-link"></a>
+                    <div className="testimonials-avatar">
+                      <h3>Sweths Shankar</h3>
+                      <h4>Senior Developer</h4>
+                    </div>
+                  </div>
+                  <div className="testimonials-text-after"><i className="fa fa-quote-left"></i></div>
+                </div>
+              </div>
+
+              <div className="swiper-slide">
+                <div className="testi-item">
+                  <div className="testi-avatar"><img src='https://avatars.githubusercontent.com/u/89839141?s=64&v=4' /></div>
+                  <div className="testimonials-text-before"><i className="fa fa-quote-right"></i></div>
+                  <div className="testimonials-text">
+                    <div className="listing-rating">
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                    </div>
+                    <p>Hi, all I am from Nepal and I am working here since a year.I would suggest you to join this company as it gives you emmence opportunity to explore yourself beyond your limit.The employee here are very co-perative.I joined this company as a trainee and now I can proudly say that I am professional coder.</p>
+                    <a href="#" className="text-link"></a>
+                    <div className="testimonials-avatar">
+                      <h3>Bijay Shankar</h3>
+                      <h4>Software Developer</h4>
+                    </div>
+                  </div>
+                  <div className="testimonials-text-after"><i className="fa fa-quote-left"></i></div>
+                </div>
+              </div>
+
+              <div className="swiper-slide">
+                <div className="testi-item">
+                  <div className="testi-avatar"><img src='https://avatars.githubusercontent.com/u/84720649?s=400&u=038102d16f44b849c0b6280c3521590b25776fa3&v=4' /></div>
+                  <div className="testimonials-text-before"><i className="fa fa-quote-right"></i></div>
+                  <div className="testimonials-text">
+                    <div className="listing-rating">
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                      <i className="fa fa-star"></i>
+                    </div>
+                    <p>Started my career as a junior software developer and learnt everything at here  and my continuous efforts and organization opportunities helped me to got a chance to work in various project ann that helped me a lot in my interpersonal skills n changed my life. com</p>
+                    <a href="#" className="text-link"></a>
+                    <div className="testimonials-avatar">
+                      <h3>Ganesh Dimri</h3>
+                      <h4>Software Developer</h4>
+                    </div>
+                  </div>
+                  <div className="testimonials-text-after"><i className="fa fa-quote-left"></i></div>
+                </div>
+              </div>
+
+              <div className="swiper-slide">
+                <div className="testi-item">
                   <div className="testi-avatar"><img src={image1} /></div>
                   <div className="testimonials-text-before"><i className="fa fa-quote-right"></i></div>
                   <div className="testimonials-text">
@@ -92,80 +158,12 @@ const App = () => {
                       <i className="fa fa-star"></i>
                       <i className="fa fa-star"></i>
                     </div>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p>Enjoyed my life to the fullest while I was working here for a more than a year.I missed all my former employee especially swetha as she was the one who was always by my side whenever I had some doubt regarding my technical skill.I would recommend you to join this company as you will get lot to learn from the friendly enviroment provided by the management.I wish I would had extended my stay here but this was the perfect time to leave as I want to challenge my-self and explore the vast amount of technical knowledge so that I can achieve my further goal.At last wanted to say that this company was my first step in achieving my dream goal.Thank a lot</p>
                     <a href="#" className="text-link"></a>
                     <div className="testimonials-avatar">
-                      <h3>John Doe</h3>
-                      <h4>Owner</h4>
-                    </div>
-                  </div>
-                  <div className="testimonials-text-after"><i className="fa fa-quote-left"></i></div>
-                </div>
-              </div>
-
-              <div className="swiper-slide">
-                <div className="testi-item">
-                  <div className="testi-avatar"><img src={image2} /></div>
-                  <div className="testimonials-text-before"><i className="fa fa-quote-right"></i></div>
-                  <div className="testimonials-text">
-                    <div className="listing-rating">
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                    </div>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <a href="#" className="text-link"></a>
-                    <div className="testimonials-avatar">
-                      <h3>Doe Boe</h3>
-                      <h4>Director</h4>
-                    </div>
-                  </div>
-                  <div className="testimonials-text-after"><i className="fa fa-quote-left"></i></div>
-                </div>
-              </div>
-
-              <div className="swiper-slide">
-                <div className="testi-item">
-                  <div className="testi-avatar"><img src={image3} /></div>
-                  <div className="testimonials-text-before"><i className="fa fa-quote-right"></i></div>
-                  <div className="testimonials-text">
-                    <div className="listing-rating">
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                    </div>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <a href="#" className="text-link"></a>
-                    <div className="testimonials-avatar">
-                      <h3>Boe Doe</h3>
-                      <h4>Developer</h4>
-                    </div>
-                  </div>
-                  <div className="testimonials-text-after"><i className="fa fa-quote-left"></i></div>
-                </div>
-              </div>
-
-              <div className="swiper-slide">
-                <div className="testi-item">
-                  <div className="testi-avatar"><img src={image4} /></div>
-                  <div className="testimonials-text-before"><i className="fa fa-quote-right"></i></div>
-                  <div className="testimonials-text">
-                    <div className="listing-rating">
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                    </div>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <a href="#" className="text-link"></a>
-                    <div className="testimonials-avatar">
-                      <h3>Doe John</h3>
-                      <h4>Designer</h4>
+                      <h3>Syed Hussain Pasha</h3>
+                      <h4>Former Employee</h4>
+                      <h4>Senior Software Developer(Tech Mahindra)</h4>
                     </div>
                   </div>
                   <div className="testimonials-text-after"><i className="fa fa-quote-left"></i></div>
